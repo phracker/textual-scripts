35fce562fe4b12f13e69746919a5675c54708ca1
the baddest motherfucker <phracker@privatdemail.net>
2014-10-30 19:34:16 -0400

    Update dns.py
    Merged Chris McCoy's suggestion from https://github.com/phracker/textual-scripts/issues/3

--- dns.py
+++ dns.py
@@ -5,7 +5,7 @@ import socket
 
 if __name__ == '__main__':
     if sys.argv[2] != '':
-        result = socket.gethostbyaddr(sys.argv[2])
+        result = socket.gethostbyname(sys.argv[2])
         print('{0}: {1} ({2})'.format(sys.argv[2], result[0], result[2][0]))
     else:
         print('/debug Usage: /dns <Domain Name or IP Address>')
